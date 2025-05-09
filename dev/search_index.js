4e6370b8f3abfb725368c3af7adc09451ba3549f
Documenter.jl <documenter@juliadocs.github.io>
2025-05-09 12:29:35 +0000

    build based on 548916a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Master*Thesis*Paulogcd_2025","page":"Home","title":"MasterThesisPaulogcd_2025","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MasterThesisPaulogcd_2025.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#Master_Thesis_Paulogcd_2025.test-Tuple{}","page":"Home","title":"Master_Thesis_Paulogcd_2025.test","text":"The test function ensures the package is well loaded.\n\n\n\n\n\n","category":"method"}]
+}
